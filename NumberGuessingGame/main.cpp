d7160c7aa1b441b86a1426060898b547efadc749
mikeLofton <michaellofton32@gmail.com>
2022-01-19 15:36:05 -0600

    Change structure

--- NumberGuessingGame/main.cpp
+++ NumberGuessingGame/main.cpp
@@ -8,50 +8,49 @@ int main()
 	srand(time(0));
 	int lowerBound = 1;
 	int upperBound = 100;
+	int range = upperBound - lowerBound;
 
+	//Tell user to think of a number within range
 	std::cout << "Think of a number between " << lowerBound << " and " << upperBound << std::endl;
 	system("PAUSE");
-	while (!gameOver)
-	{
-		
-		int input;
 
+	while (!gameOver)
+	{	
+		int input = 0;
 		
-		int numToSelect = rand() % (upperBound - lowerBound) + lowerBound;
+		int numToSelect = (rand() % range) + lowerBound;
 
 		std::cout << numToSelect << std::endl;
-
-		std::cout << "Is this you number?\n1: YES\n2: NO" << std::endl;
-		std::cin >> input;
-
-		if (input == 1)
-		{
-			system("cls");
-			std::cout << "I GOT IT!" << std::endl;
-			gameOver = true;
-			break;
-		}
 		
-		std::cout << "Is your number lower or higher?\n1: LOWER\n2: HIGHER" << std::endl;
+		//Ask if number is lower, higher, or equal
+		std::cout << "Is your number lower, higher, or equal?\n1: LOWER\n2: HIGHER\n3: EQUAL" << std::endl;
 		std::cin >> input;
 
 		if (input == 1)
 		{
 			upperBound = numToSelect - 1;
 		}
-
-		if (input == 2)
+		else if (input == 2)
 		{
 			lowerBound = numToSelect + 1;
 		}
-		
-		if (lowerBound == upperBound)
+		else if (input == 3)
+		{
+			system("cls");
+			std::cout << "I GOT IT!" << std::endl;
+			gameOver = true;
+			break;
+		}
+
+		range = upperBound - lowerBound;
+		if (range <= 0)
 		{
 			std::cout << "CHEATER! Start Over" << std::endl;
 			gameOver = true;
 			break;
 		}
 
+		
 		system("cls");
 
 	}
